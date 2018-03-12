233156d3baff8cbf3964a9e060ad02c9f246069a
thegoodfellas <37139815+thegoodfellas@users.noreply.github.com>
2018-03-12 16:00:11 +0530

    Add files via upload

---
+++ Client_Side/script_signup.js
@@ -0,0 +1,16 @@
+<script language= "javascript" type="text/javascript">
+  var samp_str = "Here are some escaped charactrers.";
+  document.write(samp_str);
+
+      var crypto = window.crypto || window.msCrypto;
+      if(crypto.subtle)
+      {
+          alert("Cryptography API Supported");
+
+        }
+        else alert("Not Supported");
+
+</script>
+<noscript>
+  <h3><This site requires JavaScript.></h3>
+</noscript>
